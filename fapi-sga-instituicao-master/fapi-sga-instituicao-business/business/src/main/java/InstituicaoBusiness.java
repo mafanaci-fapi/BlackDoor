79ba48b819d5fa308a4ddef879947086929c0331
Kevin Volanski <kvolanski@gmail.com>
2018-06-29 12:03:12 -0300

    Construindo Business

---
+++ fapi-sga-instituicao-master/fapi-sga-instituicao-business/business/src/main/java/InstituicaoBusiness.java
@@ -0,0 +1,16 @@
+import br.edu.fapi.sga.model.instituicao.Instituicao;
+
+import java.util.List;
+
+public interface InstituicaoBusiness {
+
+    int cadastrarInstituicao(Instituicao instituicao);
+
+    Instituicao pesquisarInstituicao (Instituicao instituicao);
+
+    List<Instituicao> pesquisarListaInstituicao (Instituicao instituicao);
+
+    int atualizarInstituicao (Instituicao instituicao);
+
+    int excluirInstituicao(Instituicao instituicao);
+}
